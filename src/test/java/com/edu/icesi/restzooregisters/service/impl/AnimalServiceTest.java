9b6e084fb5998cd817c950c7c061b67681dc1598
Juan David Cruz Garcia <angrybirdsgallo@gmail.com>
2022-10-08 00:08:47 -0500

    Update AnimalServiceTest.java

--- src/test/java/com/edu/icesi/restzooregisters/service/impl/AnimalServiceTest.java
+++ src/test/java/com/edu/icesi/restzooregisters/service/impl/AnimalServiceTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static com.edu.icesi.restzooregisters.constants.GenericTurtles.*;
@@ -23,6 +25,7 @@ public class AnimalServiceTest {
     private String animalName;
     private Animal animal;
     private Animal updatedAnimal;
+    private Animal femaleAnimal;
 
     @BeforeEach
     private void init() {
@@ -54,7 +57,29 @@ public class AnimalServiceTest {
         updatedAnimal=new Animal(UUID.randomUUID(),name,sex,weight,age,height,arrivalDate,fatherID,motherID);
     }
 
+    private void setupScene3(){
+        String name = "Camila";
+        char sex='F';
+        double weight=140;
+        int age=17;
+        double height=47;
+        LocalDateTime arrivalDate= TURTLE_DATE;
+        UUID fatherID=GENERIC_MALE_ID;
+        UUID motherID=GENERIC_FEMALE_ID;
+        femaleAnimal=new Animal(UUID.randomUUID(),name,sex,weight,age,height,arrivalDate,fatherID,motherID);
+    }
+
+    private List<Animal> fakeAnimalList(){
+        setupScene1();
+        List<Animal> fakeAnimals = new ArrayList<>();
+        fakeAnimals.add(animal);
+        return  fakeAnimals;
+    }
+
+
+
     private boolean createGeneratesException(){
+        setupScene1();
         try {
             animalService.createAnimal(animal);
         }
@@ -64,12 +89,49 @@ public class AnimalServiceTest {
         return false;
     }
 
+    private boolean updateGeneratesException(){
+        setupScene2();
+        when(animalRepository.findAll()).thenReturn(fakeAnimalList());
+        try {
+            animalService.updateAnimal(animalName,updatedAnimal);
+        }
+        catch (Exception e){
+            return true;
+        }
+        return false;
+    }
+
     @Test
-    public void verifyCreateAnimal(){
-        setupScene1();
+    public void testCreateAnimal(){
         assertFalse(createGeneratesException());
         verify(animalRepository,times(1)).save(any());
     }
 
-    
+    @Test
+    public void testUpdateAnimal(){
+        assertFalse(updateGeneratesException());
+        verify(animalRepository,times(1)).save(any());
+    }
+
+    @Test
+    public void testGetAnimal() {
+        setupScene1();
+        when(animalRepository.findAll()).thenReturn(fakeAnimalList());
+        List<Animal> obtainedAnimal = animalService.getAnimal(animalName);
+        verify(animalRepository, times(1)).findAll(); //The animal itself
+        verify(animalRepository, times(2)).findById(any()); //Two calls, it's father and it's mother
+    }
+
+    @Test
+    public void testGetAnimals() {
+        animalService.getAnimals();
+        verify(animalRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void validateFatherCreation(){
+        //WIP
+    }
+
+
 }
